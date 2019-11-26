2e49ba5265af1b11dbe12c8e0f68c48cc6dfe135
tsrgit <trgit@hoghill.net>
2019-11-25 23:42:14 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# tweakplaylist
-Enhanced Playlist
+# Enhanced Playlist
+Module to enhance various functions of Foundry playlists:
+* Only display playlists to the GM.
+* New parameters for sounds in playlists allow for random loop delays and random volume adjustments between repeated plays of the sound.
+* CSS styles to add color to Play and Stop buttons in playlists.
+
+Patches rely on the **patchClass** utility function.
+
+# Installation
+See https://github.com/foundry-vtt-community/wiki/wiki/Modules#installing-modules. Open the Add-on Modules tab in the Configuration and Setup dialog. Click Install Module, paste `https://raw.githubusercontent.com/trdischat/playlist/master/module.json` in as the Manifest URL, then click Install.
+
+As DM go to the `Manage Modules` options menu in the Game Settings for your World, then enable the `Enhanced Playlist` module.
+
+# License
+This Foundry VTT module, written by trdischat with major assistance from KaKaRoTo, is licensed under a [Creative Commons Attribution 4.0 International License](http://creativecommons.org/licenses/by/4.0/).
+
+This work is licensed under Foundry Virtual Tabletop [EULA - Limited License Agreement for module development v 0.1.6](http://foundryvtt.com/pages/license.html).
---
+++ scripts/patchSound.js
@@ -0,0 +1,89 @@
+/**
+ * Utility function used by patch functions to alter specific lines in a class
+ * @param {Class} klass           Class to be patched
+ * @param {Function} func         Function in the class to be patched
+ * @param {Number} line_number    Line within the function to be patched
+ * @param {String} line           Existing text of line to be patched
+ * @param {String} new_line       Replacement text for line to be patched
+ * @returns {Class}              Revised class
+ */
+function patchClass(klass, func, line_number, line, new_line) {
+  let funcStr = func.toString()
+  let lines = funcStr.split("\n")
+  if (lines[line_number].trim() == line.trim()) {
+    lines[line_number] = lines[line_number].replace(line, new_line);
+    classStr = klass.toString()
+    fixedClass = classStr.replace(funcStr, lines.join("\n"))
+    return Function('"use strict";return (' + fixedClass + ')')();
+  }
+  else {
+    console.log("Function has wrong content at line ", line_number, " : ", lines[line_number].trim(), " != ", line.trim(), "\n", funcStr)
+  }
+}
+
+/**
+ * Patch PlaylistDirectory class to display playlists only to the GM
+ */
+function patchPlaylistDirectoryClass() {
+    newClass = patchClass(PlaylistDirectory, PlaylistDirectory.prototype.getData, 4,
+      `let visible = game.playlists.entities.filter(p => isGM || p.sounds.some(s => s.playing));`,
+      `let visible = game.playlists.entities.filter(p => isGM);`);
+    if (!newClass) return;
+    PlaylistDirectory = newClass
+}
+
+if (patchedPlaylistDirectoryClass == undefined) {
+  patchPlaylistDirectoryClass();
+  var patchedPlaylistDirectoryClass = true;
+}
+
+/**
+ * Patch Playlist class to implement random loop delays and random volume adjustments
+ */
+function patchPlaylistClass() {
+    newClass = patchClass(Playlist, Playlist.prototype._onEnd, 5,
+      `if ( sound.repeat ) return;`,
+      `if ( sound.repeat ) {
+      if ( sound.flags.mindelay === undefined || sound.flags.maxdelay === undefined) return;
+      else {
+        let p = this;
+        p.updateSound({id: sound.id, playing: false});
+        let tMin = sound.flags.mindelay;
+        let tMax = sound.flags.maxdelay;
+        let tDelay = Math.floor( 1000 * ( tMin + ( Math.random() * (tMax-tMin) ) ) );
+        setTimeout( function() { p.updateSound({id: sound.id, playing: true}); }, tDelay);
+        return;}}`);
+    if (!newClass) return;
+    newClass = patchClass(newClass, Playlist.prototype.playSound, 12,
+      `let vol = sound.volume * game.settings.get("core", "globalPlaylistVolume");`,
+      `let minVol = ( sound.flags.minvolume === undefined ) ? 1.0 : parseFloat(sound.flags.minvolume);
+      let volAdj = ( minVol === 1.0 ) ? 1.0 : Math.pow( minVol + ( Math.random() * ( 1 - minVol )), 2);
+      let vol = sound.volume * game.settings.get("core", "globalPlaylistVolume") * volAdj;`);
+    if (!newClass) return;
+    Playlist = newClass
+}
+
+if (patchedPlaylistClass == undefined) {
+  patchPlaylistClass();
+  var patchedPlaylistClass = true;
+}
+
+/**
+ * Use custom template for PlaylistSoundConfig to configure random delays and random volume adjustments 
+ */
+replaceDefaultOptions = function(class_) {
+  defaultOptionsProperty = Object.getOwnPropertyDescriptor(class_, "defaultOptions");
+  if (defaultOptionsProperty == undefined) {
+    defaultOptionsProperty = Object.getOwnPropertyDescriptor(FormApplication, "defaultOptions");
+  }
+  Object.defineProperty(class_, '_sound_config_defaultOptions', defaultOptionsProperty);
+  Object.defineProperty(class_, 'defaultOptions', {
+    get: function () {
+      def_options = class_._sound_config_defaultOptions;
+      def_options.template = def_options.template.replace("templates/playlist/", "modules/playlist/templates/")
+      return def_options
+    }
+  });
+};
+
+replaceDefaultOptions(PlaylistSoundConfig);
